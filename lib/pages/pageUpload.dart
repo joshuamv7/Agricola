29923e600052fa76f309663f098d3933ee79c21c
joshua <joshua.moreira7@gmail.com>
2021-11-21 14:12:58 -0600

    cofiguration

--- lib/pages/pageUpload.dart
+++ lib/pages/pageUpload.dart
@@ -5,6 +5,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:path/path.dart';
+import 'package:flutter/src/rendering/box.dart';
 
 class pageUpload extends StatefulWidget {
   @override
@@ -13,22 +14,141 @@ class pageUpload extends StatefulWidget {
 
 class _pageUploadState extends State<pageUpload> {
   final database = FirebaseDatabase.instance.reference();
-  String URL = 'https://googleflutter.com/sample_image.jpg';
+
+  String plantName = "";
+  String email = "";
   UploadTask? task;
   File? file;
   @override
   Widget build(BuildContext context) {
+    final namePlant = database.child('/plantName');
+    final userEmail = database.child('/email');
     final fileName = file != null ? basename(file!.path) : 'No File Selected';
     return Column(children: <Widget>[
-      ElevatedButton(
-        onPressed: selectFile,
-        child: Text("Seleccionar"),
+      Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Text(
+          "CONFIGURATIONS",
+          style: TextStyle(fontSize: 40),
+        ),
+        alignment: Alignment.topLeft,
       ),
-      ElevatedButton(
-        onPressed: uploadFile,
-        child: Text("Subir"),
+      Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "SELECT IMAGE FOR PLANT: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          ElevatedButton(
+            onPressed: selectFile,
+            child: Text("SELECT IMAGE"),
+          ),
+        ],
       ),
-      Image.network(URL),
+      Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "UPLOAD SELECTED IMAGE: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          ElevatedButton(
+            onPressed: uploadFile,
+            child: Text("UPLOAD IMAGE"),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "PLANT NAME: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+        ],
+      ),
+      TextField(
+        decoration: InputDecoration(hintText: "PLANT NAME"),
+        onChanged: (String str) {
+          setState(() {
+            plantName = str;
+          });
+        },
+      ),
+      Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "UPLOAD PLANT NAME: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                await namePlant.set({"name": plantName});
+              } catch (e) {
+                print('You got an error!  $e');
+              }
+            },
+            child: Text("UPLOAD PLANT NAME"),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "USER EMAIL: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+        ],
+      ),
+      TextField(
+        decoration: InputDecoration(hintText: "USER EMAIL"),
+        onChanged: (String str) {
+          setState(() {
+            email = str;
+          });
+        },
+      ),
+      Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "UPLOAD USER EMAIL: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                await userEmail.set({"email": email});
+              } catch (e) {
+                print('You got an error!  $e');
+              }
+            },
+            child: Text("UPLOAD PLANT NAME"),
+          ),
+        ],
+      ),
     ]);
   }
 
